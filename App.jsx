2e9a0b6039b1ee2e6bb7451ee0ae4f3b3dbb01b8
rickyroses <39735158+rickyroses@users.noreply.github.com>
2024-11-23 21:38:39 -0500

    Add files via upload
    Agregacion Modals

--- App.jsx
+++ App.jsx
@@ -1,15 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import MovieList from './MovieList';
+import Modal from 'react-modal';
+import MovieModal from './MovieModal'; 
 
-export default function App() {
+Modal.setAppElement('#root'); 
+
+function App() {
   const [movies, setMovies] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
+  const [selectedMovie, setSelectedMovie] = useState(null);   
+
+  const [isModalOpen, setIsModalOpen] = useState(false); 
 
   useEffect(() => {
     const fetchMovies = async () => {
-      const response = await fetch(  
-
+      const response  = await fetch(
         `https://api.themoviedb.org/3/movie/popular?api_key=4f573ea0aa920d51a123405a7ffd7a5c&language=es-ES`
       );
       const data = await response.json();
@@ -21,6 +27,15 @@ export default function App() {
 
   const handleSearchChange = (event) => {
     setSearchTerm(event.target.value);
+  };
+
+  const handleMovieSelect = (movie) => {
+    setSelectedMovie(movie);
+    setIsModalOpen(true); 
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false); 
   };
 
   return (
@@ -35,8 +50,19 @@ export default function App() {
         />
       </header>
       <main>
-        <MovieList movies={movies} searchTerm={searchTerm} />
+        <MovieList 
+          movies={movies} 
+          searchTerm={searchTerm} 
+          onMovieSelect={handleMovieSelect} 
+        />
+        <MovieModal 
+          isOpen={isModalOpen} 
+          onClose={closeModal} 
+          movie={selectedMovie} 
+        /> 
       </main>
     </div>
   );
 }
+
+export default App;
--- MovieCard.jsx
+++ MovieCard.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
-export default function MovieCard({ movie }) {
+
+export default function MovieCard({ movie, onMovieSelect }) {
   const imageUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
 
   return (
-    <li>
+    <li onClick={() => onMovieSelect(movie)}> 
       <img src={imageUrl} alt={movie.title} />
       <h3>{movie.title}</h3>
     </li>
